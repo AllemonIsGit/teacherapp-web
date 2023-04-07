95d3032c844dba4236cfab78df21dfab8994871e
Allemon <allemon3@gmail.com>
2023-04-07 18:53:09 +0200

    styled login form inputs

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
@@ -13,11 +13,13 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup
   tokenService = new JwtHelperService()
+  username: string
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private renderer: Renderer2
   ) { }
 
   ngOnInit(): void {
@@ -42,11 +44,30 @@ export class LoginComponent implements OnInit {
   }
 
   onRegisterClick() {
-    
+    this.router.navigate([`/register`])
   }
 
   onLoginClick() {
+    console.log(this.loginForm.getRawValue().username)
+  }
+
+  changeColor() {
+    var input = this.renderer.selectRootElement(`#inputUsername`)
+    this.renderer.addClass(input, `myInputInvalid`)
+    input = this.renderer.selectRootElement(`#inputPassword`)
+    this.renderer.addClass(input, `myInputInvalid`)
+  }
+
+  onInputChange() {
+    if (!this.loginForm.get('username')?.valid) {
+      this.changeColor()
+    } else {
+      const input = this.renderer.selectRootElement(`#inputUsername`)
+      this.renderer.removeClass(input, "myInputInvalid")
+    }
 
   }
+
+  
 
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+  registerForm: FormGroup
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthenticationService
+  ) {  }
+
+  ngOnInit(): void {
+    
+    this.registerForm = this.formBuilder.group({
+      username: ['', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20)
+      ]],
+      nickname: ['', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20)
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(20)
+      ]],
+      rePassword: ['', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(20)
+      ]]
+    })
+  }
+
+
+  onBackToLoginClick() {
+    this.router.navigate([``])
+  }
+
+  onSubmit() {
+
+  }
 
 }
